f755b804a1a3570ac733330172139bf712a3400f
Danikileitor <focusyi@hotmail.com>
2023-11-26 13:18:43 +0100

    initial

--- public/src/entities/player.js
+++ public/src/entities/player.js
@@ -1,7 +1,7 @@
 class player {
   constructor() {
     this.pos = new position(this, 300, 300);
-    this.img = new graphics(this, this.pos, "boy_run");
+    this.img = new animator(this, this.pos, "boy_run", 4, 4);
 
     this.properties = [
       this.pos,
