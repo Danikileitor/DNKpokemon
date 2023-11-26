4e7da4300d94c8cfeb8adee13232c848d2843192
Danikileitor <focusyi@hotmail.com>
2023-11-26 14:37:53 +0100

    initial

--- public/src/main.js
+++ public/src/main.js
@@ -5,14 +5,10 @@ function setup() {
   new rawImage("Characters", "boy_run");
 
   new imageSet("Tilesets", "Outside", 8, 502);
+  new rawMap("testMap");
 
   new player();
 }
 function step() {
-  for (let x = 0; x < 1920; x += 32) {
-    for (let y = 0; y < 1080; y += 32) {
-      var p = new position(null, x, y);
-      imageSet.draw("Outside", p, 1, 0);
-    }
-  }
+  drawMap("testMap");
 }
--- public/src/properties/graphics/rawMap.js
+++ public/src/properties/graphics/rawMap.js
@@ -15,3 +15,14 @@ class rawMap {
         rawMaps[name] = this;
     }
 }
+
+function drawMap(name) {
+    var map = rawMaps[name];
+    if (!map.loaded) return;
+    for (let x = 0; x < 1920; x += 32) {
+        for (let y = 0; y < 1080; y += 32) {
+            var p = new position(null, x, y);
+            imageSet.draw(map.raw["tileset"], p, map.raw["backgroundTile"].x, map.raw["backgroundTile"].y);
+        }
+    }
+}
