8d31f39ba3278a180b721286320b2f843fa4ca65
Danikileitor <focusyi@hotmail.com>
2023-11-26 13:28:35 +0100

    initial

--- public/src/main.js
+++ public/src/main.js
@@ -2,7 +2,10 @@ client.init("localhost:3000");
 
 function setup() {
   Screen.init();
-  new rawImage("Characters/", "boy_run");
+  new rawImage("Characters", "boy_run");
+
+  new imageSet("Tilesets", "Outside", 8, 501);
+
   new player();
 }
 function step() {
--- public/src/properties/graphics/rawImage.js
+++ public/src/properties/graphics/rawImage.js
@@ -1,7 +1,7 @@
 var rawImages = {};
 
 class rawImage {
-  constructor(dir, file, name) {
+  constructor(file, name) {
     if (!name) {
       name = file;
       file = "";
@@ -9,7 +9,7 @@ class rawImage {
     this.file = file;
     this.name = name;
     this.dir = dir;
-    this.raw = loadImage("res/Graphics/" + dir + file + name + ".png", () => {
+    this.raw = loadImage("res/Graphics/" + file + name + ".png", () => {
       this.loaded = true;
       this.width = this.raw.width;
       this.height = this.raw.height;
