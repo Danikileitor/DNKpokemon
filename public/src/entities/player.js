9e361d868ad5ef31c8f25d5091b8d43ff12f04b3
Danikileitor <focusyi@hotmail.com>
2023-11-26 16:11:59 +0100

    initial

--- public/src/entities/player.js
+++ public/src/entities/player.js
@@ -2,15 +2,13 @@ class player {
   constructor() {
     this.pos = new position(this, 300, 300);
     this.img = new animator(this, this.pos, "boy_run", 4, 4, 0.1);
+    this.controller = new userMovement(this, this.pos, this.img);
 
     this.properties = [
       this.pos,
-      this.img
+      this.img,
+      this.controller
     ];
     objects.push(this);
-  }
-  step() {
-    this.pos.y += 2;
-    this.img.animateX();
   }
 }
